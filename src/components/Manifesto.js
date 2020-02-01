e85b3a954dc401f625d409ca0b7f804853b3da63
PianoFF <Fangfei1star@gmail.com>
2020-02-01 19:35:20 +0000

    manifesto in poisition

--- src/components/Manifesto.js
+++ src/components/Manifesto.js
@@ -1,9 +1,30 @@
 import React from "react"
+import "./Manifesto.css"
 
 export const Manifesto = () => (
   <p>
-    <img src="assets/collab-logo.png"></img>
+    <img src="collab-logo.png" className="collab-logo sepia"></img> <br />
     Manifesto: <br />
-    test
+    <strong>
+      <i>Collab</i>
+    </strong>
+    is dedicated to provide an easy and friendly platform for modern musician
+    to:
+    <ul>
+      <li> be connected to other musician around a given location</li>
+      <li> be able to post hiring information</li>
+    </ul>
+    <strong>
+      {" "}
+      <i> Collab</i>
+    </strong>{" "}
+    is not responsible to regulate this sacred and special market <br />
+    but only aims to provide the bridge to connect one's need to a possible
+    solution. <br />
+    All transactions happen amongst individuals are personal behavior. All{" "}
+    <strong>
+      <i> Collab </i>
+    </strong>{" "}
+    has to offer is only available to registered users.
   </p>
 )
